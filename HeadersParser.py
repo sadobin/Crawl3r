b401d5d11aaf338764477e3755ff41651fc9ad2a
sadobin <mr.koochaki99@gmail.com>
2021-04-12 16:07:51 +0430

    update ResponseHeadersParser: implement fetch_header method to indexing specified response headers

--- HeadersParser.py
+++ HeadersParser.py
@@ -2,14 +2,15 @@
 
 from UserAgentParser import UserAgentParser
 from PageScraper import PageScraper
+from config import REQUEST_HEADERS, RESPONSE_HEADERS
 import json
 
 
 class RequestHeadersParser:
 
-    def __init__(self, host, headers):
+    def __init__(self, host):
         self.host    = host
-        self.headers = headers
+        self.headers = REQUEST_HEADERS
 
         self.headers_parser()
 
@@ -39,12 +40,25 @@ class ResponseHeadersParser:
     def __init__(self, res_headers):
 
         """
-            Define desired response headers to be indexed.
+            Define desired response headers to be indexed in RESPONSE_HEADERS.
+            All response headers would be indexed if the RESPONSE_HEADERS has no value.
         """
 
-        # Nothing to do 
-        self.res_headers = res_headers
+        self.headers = {}
+
+        if RESPONSE_HEADERS and RESPONSE_HEADERS[0]:
+            for header in RESPONSE_HEADERS:
+                self.headers.update( self.fetch_header(res_headers, header) )
+        else:
+            self.headers = res_headers
+
+
+    def fetch_header(self, res_headers, header_name):
+        if res_headers.get(header_name):
+            return { header_name: res_headers.get(header_name) }
+        else:
+            return {}
 
 
     def get_headers(self):
-        return self.res_headers
+        return self.headers
