e2971b43b5fbf270b2fcc574b6d51034b7029a2b
sadobin <komoh@protonmail.com>
2023-11-18 02:15:31 +0330

    add create home directory in this class

--- src/OutputHandler.py
+++ src/OutputHandler.py
@@ -1,45 +1,50 @@
 #! /usr/bin/python3
 
 import time
-import datetime
+from datetime import datetime
 import json
 from collections import Counter
-import os, sys
-sys.path.append( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) )
-
+import os
+import sys
+import subprocess
+import logging
 import config
+
+logging.basicConfig(level=logging.INFO)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 
 class OutputHandler:
 
-
     def __init__(self, hostname):
         self.hostname = hostname
-        self.dir = config.RESULT_PATH
-        self.date = datetime.datetime.fromtimestamp( time.time() ).strftime("%y-%m-%d")
-
-        log_file = f'{self.dir}/logger.{self.hostname}.{self.date}.log'
-        self.log_file = open(log_file, 'w')
-
-
+        self.dir = self.create_target_dir(hostname)
+        # date = datetime.datetime.fromtimestamp(time.time()).strftime("%y-%m-%d")
+        date = datetime.now().strftime("%y-%m-%d")
+        
+        log_file = f'{self.dir}/logger.{self.hostname}.{date}.log'
+        self.log_file = open(log_file, 'w+')
 
     def logger(self, log):
-        Time = datetime.datetime.fromtimestamp( time.time() ).strftime("%H:%M:%S")  # Return current time
-        self.log_file.writelines(f"[ {Time} ] {log}\n")
-        print(f"\33[36m[ {Time} ]\33[0m {log}")
+        # time_ = datetime.datetime.fromtimestamp(time.time()).strftime("%H-%M-%S")  # Return current time
+        time_ = datetime.now().strftime("%H-%M-%S")  # Return current time
+        self.log_file.writelines(f"[ {time_} ] {log}\n")
+        print(f"\33[36m[ {time_} ]\33[0m {log}")
 
 
-    
-    def final_result(self, redis_pool, been_crawled):
+    def final_result(self, pg_global_pool, been_crawled):
+        # self.redis_pool = redis_pool
+        # reqer_result = self.redis_fetcher('reqer_result')
+        # reqer_result = self.prettify_reqer_result(reqer_result)
+        # all_paths = self.redis_fetcher('all_paths')
+        # all_paths = list(Counter(all_paths).keys())  # Make it unique
+        # static_files = self.redis_fetcher('static_files')
+        # static_files = list(Counter(static_files).keys())  # Make it unique
 
-        self.redis_pool = redis_pool
-
-        reqer_result = self.redis_fetcher('reqer_result')
-        reqer_result = self.prettify_reqer_result(reqer_result)
-        all_paths = self.redis_fetcher('all_paths')
-        all_paths = list( Counter(all_paths).keys() )       # Make it unique
-        static_files = self.redis_fetcher('static_files')
-        static_files = list( Counter(static_files).keys() ) # Make it unique
+        conn = pg_global_pool.create_connection()
+        reqer_result = ''
+        all_paths = ''
+        static_files = ''
 
         self.file_writer('reqer-result', reqer_result)
         self.file_writer('all-paths', all_paths)
@@ -47,14 +52,29 @@ class OutputHandler:
         self.file_writer('been-crawled', been_crawled)
 
 
+    def create_target_dir(self, hostname):
+        """
+            Path to save the result
+        """
+        now_time = datetime.now().strftime('%Y-%m-%d.%H-%M')
+        home_dir = subprocess.check_output('echo $HOME', shell=True).decode().strip()
+        if not config.CRAWLER_DIR:
+            print('[!] Set the CRAWLER_DIR in config.py')
+            sys.exit(1)
+        result_dir = f"{config.CRAWLER_DIR}/{hostname}.{now_time}"
+        if not os.path.exists(result_dir):
+            os.mkdir(result_dir)
+            logging.info(f'The result directory created ({result_dir})')
+        return result_dir
+
 
     def file_writer(self, name, data):
-        filename = f'{self.dir}/{name}.{self.hostname}.{self.date}.json'
+        time_ = datetime.now().strftime("%H-%M-%S")  # Return current time
+        filename = f'{self.dir}/{name}.{self.hostname}.{time_}.json'
         with open(filename, 'w') as f:
             data = json.JSONEncoder().encode(data)
-            data = json.loads( data )
-            json.dump( data, f, indent=4)
-
+            data = json.loads(data)
+            json.dump(data, f, indent=4)
 
 
     def prettify_reqer_result(self, reqer_result):
@@ -62,14 +82,13 @@ class OutputHandler:
         for i in reqer_result:
             data = json.loads(i)
             temp.update(dict(data))
-        
-        return temp
 
+        return temp
 
 
     def redis_fetcher(self, name):
         temp = []
         for redis_client in self.redis_pool:
             temp += redis_client.lrange(name, 0, -1)
-        
+
         return temp
