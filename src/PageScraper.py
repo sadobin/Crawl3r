111f134c4eda74269edb971b13154df4137caab5
sadobin <komoh@protonmail.com>
2024-02-08 23:39:24 +0330

    During runtime: remove comments, remove tags parser. Implement page_scraper_wrapper function in PageScraper module to find tags, comments, etc and create/save result to designated target directory structure

--- src/PageScraper.py
+++ src/PageScraper.py
@@ -6,25 +6,86 @@ sys.path.append( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) 
 from bs4 import BeautifulSoup, Comment
 from config import HTML_ATTRIBUTES, HTML_TAGS
 import re
-
+import json
+from urllib.parse import urlparse
+import subprocess
 
 
 class PageScraper:
     
-    def __init__(self, page_content):
+    def __init__(self, page_content=None, reqer_result=None, reqer_result_path=None):
 
-        self.comments = []
         self.links    = []
-        self.tags     = {}
+        if page_content:
+            self.links += self.find_links(page_content)        
+        elif reqer_result:
+            self.page_scraper_wrapper(reqer_result, reqer_result_path)
 
-        self.bs = BeautifulSoup(page_content, 'html.parser')
 
-        self.comments += self.bs.find_all(text=lambda it: isinstance(it, Comment))
-        self.links    += self.find_links(page_content)
+    def page_scraper_wrapper(self, reqer_result, reqer_result_path):
+        comments = []
+        tags = {}
+        result = {}
+        output_dir = reqer_result_path.rsplit('/', maxsplit=1)[0]
+        reqer_result = json.loads(reqer_result)
 
-        for tag in HTML_TAGS:
-            if tag:
-                self.tags.update( self.find_tag(tag) )
+        for url in reqer_result:
+            temp = {}
+            print(url)
+            bs = BeautifulSoup(reqer_result[url]['response'], 'html.parser')
+            
+            # if len(bs.contents) == 2:
+            self.file_structure(output_dir, url, str(bs.contents))
+            
+            comments += bs.find_all(text=lambda it: isinstance(it, Comment))
+            temp.update( {'comments': comments} )
+            
+            for tag in HTML_TAGS:
+                if tag:
+                    tags.update(self.find_tag(bs, tag))
+            
+            temp.update( {'tags': tags} )
+
+            result.update( {url: temp} )
+        
+        # print(json.dumps(result, indent=4))
+
+
+    def file_structure(self, path, url, content):
+        base_path = f"{path}/file-structure"
+        file_dir = ""
+        file_path = ""
+        file_name = ""
+        
+        parsed_url = urlparse(url)
+        # url_path = parsed_url.path.rsplit('/', maxsplit=1)[-1]
+        url_path = parsed_url.path
+        url_path = url_path[:-1] if url_path.endswith('/') else url_path        
+
+        if '.' in url_path.rsplit('/', maxsplit=1)[-1]:  # file has extension
+            file_dir = url_path.rsplit('/', maxsplit=1)[0]
+            file_name = url_path.rsplit('/', maxsplit=1)[-1]
+        else:
+            file_dir = url_path
+            file_name = file_dir.rsplit('/', maxsplit=1)[-1] + '.html'
+
+        file_dir  = base_path + '/' + file_dir
+        file_dir = file_dir.replace('//', '/')
+        file_path = file_dir + '/' + file_name
+        file_path +=  f'?{parsed_url.query}' if parsed_url.query else ''
+
+        print(f'-- {file_dir}')
+        print(f'-- {file_path}')
+
+        mkdir_cmd = f""" mkdir -p "{file_dir}" """
+        touch_cmd = f""" touch  "{file_path}" """
+        subprocess.run(mkdir_cmd, shell=True)
+        # os.makedirs(file_dir, exist_ok=True)
+        subprocess.run(touch_cmd, shell=True)
+
+        with open(file_path, 'w') as f:
+            f.write(content)
+
 
 
     def find_links(self, page_content):
@@ -49,14 +110,14 @@ class PageScraper:
         return links
 
 
-    def find_tag(self, tagname):
+    def find_tag(self, beautiful_soup, tagname):
         """
             Find specified tags
         """
         tags = {}
         tags[tagname] = []
 
-        for t in self.bs.find_all(tagname):
+        for t in beautiful_soup.find_all(tagname):
             tags[tagname].append(str(t))
 
         return tags
